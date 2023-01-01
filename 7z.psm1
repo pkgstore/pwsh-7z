d539fa796dfc076d72d9a176f6692b7fc39291d3
KitsuneSolar <kitsune.solar@gmail.com>
2023-01-01 16:24:15 +0300

    2023-01-01 13:24:15

--- 7z.psm1
+++ 7z.psm1
@@ -14,12 +14,12 @@ Param(
 
 function Compress-7z() {
   ForEach ($File in (Get-ChildItem $($P_Files))) {
-    & "$($env:ProgramFiles)\7-Zip\7z.exe" a "$($File.Name + '.7z')" "$($File.FullName)"
+    & "$($ENV:ProgramFiles)\7-Zip\7z.exe" a "$($File.Name + '.7z')" "$($File.FullName)"
   }
 }
 
 function Expand-7z() {
   ForEach ($File in (Get-ChildItem "$($P_Files)")) {
-    & "$($env:ProgramFiles)\7-Zip\7z.exe" x "$($File.FullName)"
+    & "$($ENV:ProgramFiles)\7-Zip\7z.exe" x "$($File.FullName)"
   }
 }
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# pwsh-7z
+# PowerShell 7z Module
+
+## Install
+
+```powershell
+git clone 'https://github.com/shellstore/pwsh-7z.git' "$($ENV:UserProfile)\Documents\PowerShell\Modules\7z";
+```
