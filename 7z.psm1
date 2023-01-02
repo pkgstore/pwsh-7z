7c40e8ff8bf509af6fa2e507722081796e237db6
KitsuneSolar <kitsune.solar@gmail.com>
2023-01-03 00:15:07 +0300

    2023-01-02 21:15:07

--- 7z.psm1
+++ 7z.psm1
@@ -3,28 +3,38 @@ function Compress-7z() {
     .SYNOPSIS
       Compress '7z' archive.
     .DESCRIPTION
+      -F
+        File list.
+      -T
+        Specifies type of archive. It can be: '7z', 'BZIP2', 'GZIP', 'TAR', 'WIM', 'XZ', 'ZIP'.
+        Default: '7z'.
+      -MX
+        Compression level (-mx1 (fastest) / ... / -mx9 (ultra)).
+        Default: 5.
+      -PWD | -P
+        Password. Encrypt both file data and headers.
   #>
 
   [CmdletBinding()]
 
   Param(
     [Parameter(Mandatory, HelpMessage="File list.")]
-    [Alias("F", "Files", "File")]
+    [Alias("F", "File", "Files")]
     [string[]]$P_File,
 
-    [Parameter(HelpMessage="Specifies type of archive. It can be: '7z', 'BZIP2', 'GZIP', 'TAR', 'WIM', 'XZ', 'ZIP'.")]
+    [Parameter(HelpMessage="Specifies type of archive ('7z' / 'BZIP2' / 'GZIP' / 'TAR' / 'WIM' / 'XZ' / 'ZIP'). Default: '7z'.")]
     [ValidateSet("7z", "BZIP2", "GZIP", "TAR", "WIM", "XZ", "ZIP")]
     [Alias("T", "Type")]
     [string]$P_Type = "7z",
 
-    [Parameter(HelpMessage="Compression level (-mx1 (fastest) / ... / -mx9 (ultra)).")]
+    [Parameter(HelpMessage="Compression level (-mx1 (fastest) / ... / -mx9 (ultra)). Default: 5.")]
     [ValidateRange(1,9)]
     [Alias("MX", "CompressionLevel", "Level")]
     [int]$P_MX = 5,
 
-    [Parameter(HelpMessage="Password.")]
-    [Alias("P", "Password")]
-    [string]$P_PWD = ""
+    [Parameter(HelpMessage="Password. Encrypt both file data and headers.")]
+    [Alias("PWD", "P", "Password")]
+    [string]$P_PWD
   )
 
   $7z = "$($PSScriptRoot)\7z.exe"
@@ -45,13 +55,15 @@ function Expand-7z() {
     .SYNOPSIS
       Expand '7z' archive.
     .DESCRIPTION
+      -F
+        File list.
   #>
 
   [CmdletBinding()]
 
   Param(
     [Parameter(Mandatory, HelpMessage="File list.")]
-    [Alias("F", "Files", "File")]
+    [Alias("F", "File", "Files")]
     [string[]]$P_File
   )
 
