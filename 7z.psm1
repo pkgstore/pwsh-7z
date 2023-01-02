e5d9ef83660d8177ec111abc42e839513e7e66b6
KitsuneSolar <kitsune.solar@gmail.com>
2023-01-02 04:13:13 +0300

    2023-01-02 01:13:13

--- 7z.psm1
+++ 7z.psm1
@@ -16,10 +16,7 @@ function Compress-7z() {
     [string[]]$P_Files
   )
 
-  $7z = "$($PSScriptRoot)\7za.exe"
-  if (Test-Path -Path "$($ENV:ProgramFiles)\7-Zip\7z.exe" -PathType "Leaf") {
-    $7z = "$($ENV:ProgramFiles)\7-Zip\7z.exe"
-  }
+  $7z = "$($PSScriptRoot)\7z.exe"
 
   ForEach ($File in (Get-ChildItem $($P_Files))) {
     & "$($7z)" a "$($File.Name + '.7z')" "$($File.FullName)"
@@ -45,9 +42,6 @@ function Expand-7z() {
   )
 
   $7z = "$($PSScriptRoot)\7z.exe"
-  if (Test-Path -Path "$($ENV:ProgramFiles)\7-Zip\7z.exe" -PathType "Leaf") {
-    $7z = "$($ENV:ProgramFiles)\7-Zip\7z.exe"
-  }
 
   ForEach ($File in (Get-ChildItem "$($P_Files)")) {
     & "$($7z)" x "$($File.FullName)"
