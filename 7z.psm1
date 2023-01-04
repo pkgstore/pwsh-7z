cb0d3398a2f43b4e676414091d44ccc5202a983c
KitsuneSolar <kitsune.solar@gmail.com>
2023-01-04 17:14:57 +0300

    2023-01-04 14:14:57

--- 7z.psm1
+++ 7z.psm1
@@ -9,34 +9,40 @@ function Compress-7z() {
         Specifies type of archive.
         Value: ['7z' | 'BZIP2' | 'GZIP' | 'TAR' | 'WIM' | 'XZ' | 'ZIP'].
         Default: '7z'.
-      -MX
+      -L
         Compression level.
         Value: [1 | ... | 9].
         Default: 5.
-      -PWD | -P
+      -P | -PWD
         Password. Encrypt both file data and headers.
+      -D | -DEL
+        Delete files after compression.
   #>
 
   [CmdletBinding()]
 
   Param(
     [Parameter(Mandatory, HelpMessage="File list.")]
-    [Alias('F', 'File', 'Files')]
-    [string[]]$P_File,
+    [Alias('Files', 'File', 'F')]
+    [string[]]$Files,
 
     [Parameter(HelpMessage="Specifies type of archive. Value: ['7z' | 'BZIP2' | 'GZIP' | 'TAR' | 'WIM' | 'XZ' | 'ZIP']. Default: '7z'.")]
     [ValidateSet('7z', 'BZIP2', 'GZIP', 'TAR', 'WIM', 'XZ', 'ZIP')]
-    [Alias('T', 'Type')]
-    [string]$P_Type = '7z',
+    [Alias('Type', 'T')]
+    [string]$Type = '7z',
 
     [Parameter(HelpMessage="Compression level. Value: [1 | ... | 9]. Default: 5.")]
     [ValidateRange(1,9)]
-    [Alias('MX', 'CompressionLevel', 'Level')]
-    [int]$P_MX = 5,
+    [Alias('Level', 'L')]
+    [int]$Level = 5,
 
     [Parameter(HelpMessage="Password. Encrypt both file data and headers.")]
-    [Alias('PWD', 'P', 'Password')]
-    [string]$P_PWD
+    [Alias('Password', 'PWD', 'P')]
+    [string]$Password,
+
+    [Parameter(HelpMessage="Delete files after compression.")]
+    [Alias('Delete', 'DEL', 'D')]
+    [switch]$Delete = $false
   )
 
   $7z = "${PSScriptRoot}\7z.exe"
@@ -45,10 +51,11 @@ function Compress-7z() {
     Write-Error -Message "'7z.exe' not found!" -ErrorAction "Stop"
   }
 
-  ForEach ( $File in ( Get-ChildItem ${P_File} ) ) {
-    $CMD = @( "a", "-t${P_Type}", "-mx${P_MX}" )
-    if ( -not ( [string]::IsNullOrEmpty(${P_PWD}) ) ) { $CMD += @( "-p${P_PWD}" ) }
-    $CMD += @( "$( ${File}.Name + '.' + ${P_Type}.ToLower() )", "$( ${File}.FullName )" )
+  ForEach ( $File in ( Get-ChildItem ${Files} ) ) {
+    $CMD = @( "a", "-t${Type}", "-mx${Level}" )
+    if ( -not ( [string]::IsNullOrEmpty(${Password}) ) ) { $CMD += @( "-p${Password}" ) }
+    if ( $Delete ) { $CMD += @( "-sdel" ) }
+    $CMD += @( "$( ${File}.Name + '.' + ${Type}.ToLower() )", "$( ${File}.FullName )" )
     & "${7z}" $CMD
   }
 }
@@ -66,8 +73,8 @@ function Expand-7z() {
 
   Param(
     [Parameter(Mandatory, HelpMessage="File list.")]
-    [Alias('F', 'File', 'Files')]
-    [string[]]$P_File
+    [Alias('Files', 'File', 'F')]
+    [string[]]$Files
   )
 
   $7z = "${PSScriptRoot}\7z.exe"
@@ -76,7 +83,7 @@ function Expand-7z() {
     Write-Error -Message "'7z.exe' not found!" -ErrorAction "Stop"
   }
 
-  ForEach ( $File in ( Get-ChildItem "${P_File}" ) ) {
+  ForEach ( $File in ( Get-ChildItem "${Files}" ) ) {
     $CMD = @( "x", "$( ${File}.FullName )" )
     & "${7z}" $CMD
   }
